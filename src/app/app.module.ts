d413f0f91891ac5095ecf51ff549a620b2d15c6c
unknown <marcone.trajano@smartitsystems.com.br>
2021-01-07 14:48:41 -0300

    Projeto Finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { AngularFireModule } from '@angular/fire'
-import { AngularFirestoreModule } from '@angular/fire/firestore'
 import { environment } from 'src/environments/environment';
 import { FormsModule } from '@angular/forms';
+
+//START - IMPORTS FIREBASE
+import { AngularFireModule } from '@angular/fire'
+import { AngularFirestoreModule } from '@angular/fire/firestore'
+//END - IMPORTS FIREBASE
 
 @NgModule({
   declarations: [AppComponent],
